529a16eec5601a546d6cf76523b8103084cb5e57
Nikhil Verma <nikhilnxvverma1@aol.com>
2019-03-16 17:48:34 -0400

    Frontend design mockup and practice

---
+++ practice/practice.js
@@ -0,0 +1,21 @@
+var population = [
+	{
+		country:"America",
+		count:300
+	},
+	{
+		country:"China",
+		count:1600
+	},
+	{
+		country:"India",
+		count:1300
+	},
+	{
+		country:"Britain",
+		count:200
+	},
+
+]
+
+var container = document.getElementById("bar-chart");
