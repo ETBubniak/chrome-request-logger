67c9f44a7b2bb6163f62e7d8679ba8304bfb6739
Evan Bubniak <evan.bubniak@gmail.com>
2019-03-16 18:46:02 -0400

    event listener successfully grabs all requests from current tab

--- backgroundScript.js
+++ backgroundScript.js
@@ -1,13 +1,43 @@
-// add event listener for a web request.
+// add event listener for a web requestDetails.
 
-
-
-const callback = function(details){
-    console.log(details);
+function getCurrentTabId(){
+    return new Promise(function(resolve, reject){
+        chrome.tabs.query({"active": true, "currentWindow": true}, (tab) => {
+            if (tab.length > 0 ){
+                resolve(tab[0].id);
+            }
+            else {
+                reject("No match");
+            }
+        });
+    });
 }
 
-const filter = {
-    "urls": new Array("http://*/*", "https://*/*")
+function isPartOfCurrentTab(request){
+    return new Promise((resolve, reject) => {
+        getCurrentTabId()
+        .then(function resolved(currentTabId){
+            if (request.tabId === currentTabId){
+                resolve(true);
+            }
+            else {
+                resolve(false);
+            }
+        }, function rejected(err){
+            resolve(false);
+        });
+    });
+}
+
+const callback = async function(request){
+    const partOfCurrentTab = await isPartOfCurrentTab(request);
+    if ((partOfCurrentTab)){
+        console.log(request);
+    }
 };
 
-chrome.webRequest.onBeforeRequest.addListener(callback, filter);
+const filter = {
+    "urls": ["http://*/*", "https://*/*"]
+};
+
+chrome.webRequest.onCompleted.addListener(callback, filter);
