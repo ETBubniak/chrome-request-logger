06fbe710e67f65b7d05614b3e32a342ee2657cce
chrisbaudouinjr <chrisbaudouinjr@icloud.com>
2019-03-17 02:25:45 -0400

    Updates count_requests function Completes save function for Collectors

--- model/collector.py
+++ model/collector.py
@@ -4,26 +4,46 @@ from dataclasses import dataclass
 from sqlite3 import Error
 
 
+def count_requests():
+    conn = sqlite3.connect("data.db")
+    cursor = conn.cursor()
+    try:
+        cursor.execute("SELECT collector_name, COUNT(*) FROM collectors GROUP BY collector_name;")
+        yield cursor.fetchall()
+    except Error as e:
+        logging.error(e)
+    finally:
+        conn.close()
+
+
 def create_table_if_none():
     conn = sqlite3.connect("data.db")
+    cursor = conn.cursor()
     try:
-        conn.execute("CREATE TABLE collectors ("
-                     "id INTEGER AUTOINCREMENT PRIMARY KEY, "
-                     "source_name VARCHAR(255), "
-                     "source_location VARCHAR(255));")
+        cursor.execute("CREATE TABLE collectors ("
+                       "id INTEGER AUTOINCREMENT PRIMARY KEY, "
+                       "collector_name VARCHAR(255), "
+                       "request_location VARCHAR(255));")
         logging.info("Successfully created table: collectors")
     except Error as e:
-        logging.warning(e)
+        logging.error(e)
     finally:
         conn.close()
 
 
 @dataclass
 class Collector:
-    source_name: str
-    source_location: str
+    collector_name: str
+    request_location: str
 
     def save(self):
         create_table_if_none()
-
-
+        conn = sqlite3.connect("data.db")
+        try:
+            conn.execute("INSERT INTO collectors (collector_name, request_location) VALUES "
+                         "'{}', '{}';".format(self.collector_name, self.request_location))
+            conn.commit()
+        except Error as e:
+            logging.error(e)
+        finally:
+            conn.close()
---
+++ server.py
@@ -0,0 +1,22 @@
+from model.collector import *
+from flask import Flask
+from flask import render_template, request, Response
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    collectors = count_requests()
+    return render_template("index.html", collectors=collectors)
+
+
+@app.route('/collector', methods=['POST'])
+def log_collector():
+    json = request.get_json()
+    collector_name = json.get('collector_name')
+    request_location = json.get('request_location')
+
+    collector = Collector(collector_name, request_location)
+    collector.save()
+
+    return Response(status=200)
